da8709c668660bb6d76ecf927b8e4747c56347a9
DanielRodriguezCUNOC <pabloalvarado202130534@cunoc.edu.gt>
2025-08-12 01:35:38 -0600

    Cambios traidos desde main

---
+++ hyrule/src/main/java/com/hyrule/Backend/Validations/ValidateParticipantRegister.java
@@ -0,0 +1,5 @@
+package com.hyrule.Backend.Validations;
+
+public class ValidateParticipantRegister {
+
+}
---
+++ hyrule/src/main/java/com/hyrule/Backend/handler/ParticipantRegisterHandler.java
@@ -0,0 +1,5 @@
+package com.hyrule.Backend.handler;
+
+public class ParticipantRegisterHandler {
+
+}
